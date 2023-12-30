35466f297845f0a5502fb55f9edb9f9defa97569
Nguyen Kim Dai <kimdai2601@gmail.com>
2023-12-30 08:48:16 +0700

    remove lang

--- src/constants/language-code.ts
+++ src/constants/language-code.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export enum LanguageCode {
   en_US = 'en_US',
-  ja_JP = 'ja_JP',
 }
 
 export const supportedLanguageCount = Object.values(LanguageCode).length;
